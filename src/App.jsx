72081fad26ab1d190ff04b2fbdb5fb01f2956795
SIDHANT <acharyasid1@gmail.com>
2023-07-23 23:48:55 +0545

    sendMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Login from "./pages/Login";
+import ChatRoom from "./pages/ChatRoom";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Login />
+      {/* <Login /> */}
+      <ChatRoom />
     </div>
   );
 }

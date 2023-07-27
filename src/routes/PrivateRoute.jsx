ff90b95f50c8e0d5edd07cbe193bff8fba7d8528
SIDHANT <acharyasid1@gmail.com>
2023-07-27 23:52:58 +0545

    privateroute

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,4 +1,10 @@
+import { Navigate } from "react-router-dom";
+
 export const PrivateRoute = ({ children }) => {
-  const currentUser = false;
-  return <div>PrivateRoute</div>;
+  const currentUser = true;
+
+  if (!currentUser) {
+    return <Navigate to="/" replace={true} />;
+  }
+  return children;
 };

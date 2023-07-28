6a472b9e9019ea6aec1e57763f4152db1513e196
SIDHANT <acharyasid1@gmail.com>
2023-07-28 21:55:38 +0545

    firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjy-Jms2OQbqPLWp-DxiNlXTXsY7SZgvU",
+  authDomain: "instantchat-72c2b.firebaseapp.com",
+  projectId: "instantchat-72c2b",
+  storageBucket: "instantchat-72c2b.appspot.com",
+  messagingSenderId: "1042656823",
+  appId: "1:1042656823:web:a1fca0e4e4c56f620699b2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
